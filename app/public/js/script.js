30709083416a89dba71f9ed60104481d88061dd2
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-20 22:32:03 +0300

    adds express files

---
+++ app/public/js/script.js
@@ -0,0 +1,5 @@
+(function () {
+
+})();
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJzY3JpcHQuanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24gKCkge1xuXG59KSgpO1xuIl19
