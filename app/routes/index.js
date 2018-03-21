8ca016b4a22930f76f6fec5dce776b5ce7b9fb9d
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-21 14:15:07 +0300

    add outpute url

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+var config = require("../config");
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "GIT-LOC" });
+  res.render("index", { title: "GIT-LOC", url: config.url });
 });
 
 module.exports = router;
