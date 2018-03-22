e29e6b394494252fc36cb2d32cdaa04c5a591380
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-22 04:59:23 +0300

    adds render branches names

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+var childProcess = require("child_process");
+
 var config = require("../config");
+var parser = require("../util/parser");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "GIT-LOC", url: config.url });
+  childProcess.exec("git branch --no-abbrev --column", {cwd: config.url}, (err, stdout, stderr) => {
+     res.render("index", {title: "GIT-LOC", url: config.url, branches: parser.branchesList(stdout)});
+      if(err) {
+          console.log('err: ' + err);
+      };
+  });
 });
 
 module.exports = router;
