5d8f9c53dd697f3c204d5c8eaae6114dbcec3529
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-22 22:31:39 +0300

    adds method parser.commitsList

--- app/util/parser.js
+++ app/util/parser.js
@@ -1,4 +1,5 @@
 var parser = {
+
   branchesList: (str) => {
     var str = str.split("");
     var branches = [];
@@ -28,6 +29,39 @@ var parser = {
     branches.unshift("master");
 
     return branches;
+  },
+
+  commitsList: (str) => {
+    var hashLenght = 40;
+    var dateLenght = 10;
+    var commits = [];
+    var number = 0;
+
+    for (var i = 0; i < str.length - hashLenght; i++) {
+      var start = str.indexOf("+++", i) + 3;
+      var end = str.indexOf("+++", start);
+      var commitStr = str.slice(start, end);
+
+      var hash = commitStr.slice(0, hashLenght);
+      commitStr = commitStr.slice(hashLenght, commitStr.lenght);
+      var date = commitStr.slice(0, dateLenght);
+      commitStr = commitStr.slice(dateLenght, commitStr.lenght);
+      var author = commitStr.slice(0, commitStr.indexOf("----", 0));
+      commitStr = commitStr.slice(commitStr.indexOf("----", 0) + 4, commitStr.lenght);
+      var message = commitStr;
+
+      commits[number] = {
+        hash: hash,
+        date: date,
+        author: author,
+        message: message
+      };
+
+      i = end + 3;
+      number++;
+    };
+
+    return commits;
   }
 };
 
