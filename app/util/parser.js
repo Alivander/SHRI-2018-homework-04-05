cbc21e485f731b6c8052b2a42f695409e9b5d4c1
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-22 04:58:57 +0300

    adds method for parser branch names

--- app/util/parser.js
+++ app/util/parser.js
@@ -1,5 +1,34 @@
 var parser = {
+  branchesList: (str) => {
+    var str = str.split("");
+    var branches = [];
+    var number = 0;
+    var pass = false;
 
+    str.forEach((s) => {
+      if (s !== "*" && s !== " ") {
+        if (pass === true) {
+          if (branches[number]) {
+            number++;
+          };
+          pass = false;
+        };
+        if (branches[number]) {
+          branches[number] += s;
+        } else {
+          branches[number] = s;
+        };
+      } else {
+        pass = true;
+      };
+    });
+
+    branches.sort();
+    delete branches[branches.indexOf("master")];
+    branches.unshift("master");
+
+    return branches;
+  }
 };
 
 module.exports = parser;
