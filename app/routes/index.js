d7c3917ea26da153bead20fd756ca73a06561b4b
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-24 01:25:40 +0300

    update routers

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 var scheme = require("./scheme");
 
-router.get("/", scheme.indexModel);
+router.get("/", scheme.index);
 
-router.get("/:branch", scheme.branchModel);
+router.get("/:branch", scheme.branch);
+
+router.get("/:branch/tree", scheme.tree);
+router.get("/:branch/tree/*", scheme.treeItem);
+router.get("/:branch/commits", scheme.commits);
+router.get("/:branch/commits/*", scheme.commit);
 
 module.exports = router;
