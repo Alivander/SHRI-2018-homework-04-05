7f0ac29ded989a2288b5df954799c684d51ec267
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-24 05:40:23 +0300

    adds routers

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,8 +7,10 @@ router.get("/", scheme.index);
 router.get("/:branch", scheme.branch);
 
 router.get("/:branch/tree", scheme.tree);
-router.get("/:branch/tree/*", scheme.treeItem);
+router.get("/:branch/tree/*", scheme.itemTree);
+
 router.get("/:branch/commits", scheme.commits);
-router.get("/:branch/commits/*", scheme.commit);
+router.get("/:branch/commits/:commit", scheme.commit);
+router.get("/:branch/commits/:commit/*", scheme.commitTree);
 
 module.exports = router;
