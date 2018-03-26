0c1d6957459ff438a66f3ecd6e76710e0297082f
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-26 16:57:10 +0300

    corrects hermione config

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,4 +1,4 @@
-const config = require('../config');
+const config = require('./app/config');
 
 module.exports = {
   baseUrl: 'http://localhost:' + config.url + '/',
@@ -9,6 +9,12 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome'
       }
+    }
+  },
+
+  plugins: {
+    'html-reporter/hermione': {
+      path: 'hermione-html-report'
     }
   }
 
