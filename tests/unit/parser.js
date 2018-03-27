81f89287ebb077dd95a560a650d4f79e95ff1b43
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-27 22:10:32 +0300

    corrects unit test

--- tests/unit/parser.js
+++ tests/unit/parser.js
@@ -20,61 +20,47 @@ describe('parser.js', () => {
   });
 
   describe('commitsList()', () => {
-    it('преобразует строку в массив объектов-коммитов', () => {
-      const fake = '7c73cbf5ee537fb3942574a119592546eeb6bb272018-03-22Alina Vanieva----update-doorOther.js\n69b7973ee1906670c2cbdcaa7aeb155b29feaa472018-03-22Alina Vanieva----update-doorBase.js\nef53d4f2c2cef5be80714f823dfe65bbd9c50a032018-03-22Alina Vanieva----update-app.js\n';
+    const fake = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a72018-03-10Alina Vanieva----Initial-commit\n69b7973ee1906670c2cbdcaa7aeb155b29feaa472018-03-22Alina Vanieva----update-doorBase.js\nef53d4f2c2cef5be80714f823dfe65bbd9c50a032018-03-22Alina Vanieva----update-app.js\n';
+    const fakeLength = 3;
 
+    it('преобразует строку в массив объектов-коммитов', () => {
       assert.isArray(parser.commitsList(fake));
-      assert.isObject(parser.commitsList(fake)[randomIndex(0, 3)]);
+      assert.isObject(parser.commitsList(fake)[randomIndex(0, fakeLength - 1)]);
     });
     it('объект-коммит содержит корректный хеш', () => {
-      const fake = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a72018-03-10Alina Vanieva----Initial-commit\n';
       const hash = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a7';
-
       assert.propertyVal(parser.commitsList(fake)[0], 'hash', hash);
     });
     it('объект-коммит содержит корректную дату', () => {
-      const fake = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a72018-03-10Alina Vanieva----Initial-commit\n';
       const date = '2018-03-10';
-
       assert.propertyVal(parser.commitsList(fake)[0], 'date', date);
     });
     it('объект-коммит содержит корректное имя автора', () => {
-      const fake = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a72018-03-10Alina Vanieva----Initial-commit\n';
       const author = 'Alina Vanieva';
-
       assert.propertyVal(parser.commitsList(fake)[0], 'author', author);
     });
     it('объект-коммит содержит корректное название коммита', () => {
-      const fake = '154cf274568ee670a1a1ec9d27a6dfffbbecc7a72018-03-10Alina Vanieva----Initial-commit\n';
       const message = 'Initial-commit';
-
       assert.propertyVal(parser.commitsList(fake)[0], 'message', message);
     });
   });
   describe('treeList()', () => {
-    it('преобразует строку в массив объектов файловой системы', () => {
-      const fake = '100644 blob 9ae1e079d86c1132f4ff8b51606a840c56d0746f\tREADME.md\n040000 tree 71689a634f855ee7bdb1ea66050216318a6c7dfd\tcss\n040000 tree 258cee0af59520f83a2d2cd6969f0a1a0be87634\timg\n100644 blob 29f3466c27d2b4890ac38621248b19c275a6048b\tindex.htmlж\n040000 tree 7cb3e902d70c5d69a079cabbc58fe967a5835e1b\tjs\n';
+    const fake = '100644 blob 9ae1e079d86c1132f4ff8b51606a840c56d0746f\tREADME.md\n040000 tree 71689a634f855ee7bdb1ea66050216318a6c7dfd\tcss\n040000 tree 258cee0af59520f83a2d2cd6969f0a1a0be87634\timg\n100644 blob 29f3466c27d2b4890ac38621248b19c275a6048b\tindex.htmlж\n040000 tree 7cb3e902d70c5d69a079cabbc58fe967a5835e1b\tjs\n';
+    const fakeLength = 5;
 
+    it('преобразует строку в массив объектов файловой системы', () => {
       assert.isArray(parser.treeList(fake));
-      assert.isObject(parser.treeList(fake)[randomIndex(0, 5)]);
+      assert.isObject(parser.treeList(fake)[randomIndex(0, fakeLength - 1)]);
     });
     it('объект содержит корректный хеш', () => {
-      const fake = '100644 blob 9ae1e079d86c1132f4ff8b51606a840c56d0746f\tREADME.md\n';
-      const hash = '9ae1e079d86c1132f4ff8b51606a840c56d0746f';
-
+      const hash = '71689a634f855ee7bdb1ea66050216318a6c7dfd';
       assert.propertyVal(parser.treeList(fake)[0], 'hash', hash);
     });
     it('объект-коммит содержит корректный тип элемента', () => {
-      const fake = '100644 blob 9ae1e079d86c1132f4ff8b51606a840c56d0746f\tREADME.md\n';
-      const type = 'blob';
-
-      assert.propertyVal(parser.treeList(fake)[0], 'type', type);
+      assert.propertyVal(parser.treeList(fake)[0], 'type', 'tree');
     });
     it('объект-коммит содержит корректное название элемента', () => {
-      const fake = '100644 blob 9ae1e079d86c1132f4ff8b51606a840c56d0746f\tREADME.md\n';
-      const name = 'README.md';
-
-      assert.propertyVal(parser.treeList(fake)[0], 'name', name);
+      assert.propertyVal(parser.treeList(fake)[0], 'name', 'css');
     });
   });
 });
