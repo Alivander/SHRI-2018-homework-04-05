079f9d10685a68efa2193646644071122bcd34c3
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-29 02:00:58 +0300

    corrects intagration test for index page

--- tests/integration/index.js
+++ tests/integration/index.js
@@ -1,42 +1,37 @@
 const { assert } = require('chai');
 const { url, host, port } = require('../../app/config');
-
-function randomIndex(min, max) {
-  /* eslint-disable no-mixed-operators */
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
-  /* eslint-anable no-mixed-operators */
-}
+const util = require('./util');
 
 /* eslint-disable no-undef, func-names */
 describe('Главная страница:', () => {
-  describe('Название:', () => {
-    it('проверка наличия title', function () {
+  describe('Название сайта:', () => {
+    it('у вкладки есть title', function () {
       return this.browser
         .url('/')
         .getTitle()
         .then((title) => {
-          assert.ok(title, 'title отсутствует');
+          assert.ok(title);
         });
     });
     it('title соответствует ожидаемому', function () {
       return this.browser
         .getTitle()
         .then((title) => {
-          assert.equal(title, 'GIT-LOC', 'некорректный title');
+          assert.equal(title, 'GIT-LOC');
         });
     });
-    it('проверка наличия h1', function () {
+    it('на странице есть заголовок h1', function () {
       return this.browser
         .getText('header h1 a')
         .then((heading) => {
-          assert.ok(heading, 'заголовок h1 отсутствует');
+          assert.ok(heading);
         });
     });
     it('заголовок h1 соответствует ожидаемому', function () {
       return this.browser
         .getText('header h1 a')
         .then((heading) => {
-          assert.equal(heading, 'GIT-LOC', 'некоректный заголовок h1');
+          assert.equal(heading, 'GIT-LOC');
         });
     });
   });
@@ -46,14 +41,14 @@ describe('Главная страница:', () => {
       return this.browser
         .$('.content__name-repo')
         .then((path) => {
-          assert.ok(path, 'не отображается url текущего репозитория');
+          assert.ok(path);
         });
     });
     it('url текущего репозитория соответствует ожидаемому', function () {
       return this.browser
         .getText('.content__name-repo')
         .then((path) => {
-          assert.equal(path, url, 'некорректный url текущего репозитория');
+          assert.equal(path, url);
         });
     });
   });
@@ -63,14 +58,14 @@ describe('Главная страница:', () => {
       return this.browser
         .$('.branches__list')
         .then((branchList) => {
-          assert.ok(branchList, 'не отображается список веток');
+          assert.ok(branchList);
         });
     });
     it('первой в списке идет ветка master', function () {
       return this.browser
         .getText('.branches__name:first-child a')
         .then((name) => {
-          assert.equal(name, 'master', 'первая ветка в списке отлична от master');
+          assert.equal(name, 'master');
         });
     });
     it('при переходе по случайной ветке отображается страница этой ветки', function () {
@@ -80,7 +75,7 @@ describe('Главная страница:', () => {
       return this.browser
         .$$('.branches__name a')
         .then((branches) => {
-          branch = branches[randomIndex(0, branches.length - 1)];
+          branch = branches[util.randomIndex(0, branches.length - 1)];
           return branch;
         })
         .getText(branch)
@@ -91,7 +86,7 @@ describe('Главная страница:', () => {
         .click()
         .getUrl()
         .then((path) => {
-          assert.equal(path, `http://${host}:${port}/${branchName}`, 'неправильная ссылка у ветки');
+          assert.equal(path, `http://${host}:${port}/${branchName}`);
         });
     });
   });
